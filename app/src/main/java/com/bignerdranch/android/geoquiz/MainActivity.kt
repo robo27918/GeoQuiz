9b48e378e54da013554cf06d76396fc00f73c53a
Robert Toribio <roberttoribio04@gmail.com>
2022-09-07 14:15:51 -0700

    Update gradle properties  Enabled androidx due to build errors

--- app/src/main/java/com/bignerdranch/android/geoquiz/MainActivity.kt
+++ app/src/main/java/com/bignerdranch/android/geoquiz/MainActivity.kt
@@ -1,12 +1,14 @@
 package com.bignerdranch.android.geoquiz
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
+
 private const val TAG = "MainActivity"
 
 class MainActivity : AppCompatActivity() {
@@ -28,6 +30,10 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         Log.d(TAG,"onCreate(Bundle?) called")
         setContentView(R.layout.activity_main)
+
+        //val provider : ViewModelProvider = ViewModelProvider(this)
+        //val quizViewModel = provider.get(QuizViewModel::class.java)
+        //Log.d(TAG, "Got a QuizViewModel: $quizViewModel")
 
         trueButton = findViewById(R.id.true_button)
         falseButton = findViewById(R.id.false_button)
--- app/src/main/java/com/bignerdranch/android/geoquiz/Question.kt
+++ app/src/main/java/com/bignerdranch/android/geoquiz/Question.kt
@@ -1,5 +1,5 @@
 package com.bignerdranch.android.geoquiz
 
-import android.support.annotation.StringRes
+import androidx.annotation.StringRes
 
-data class Question (@StringRes val textResId:Int , val answer:Boolean)
+data class Question (@StringRes val textResId:Int, val answer:Boolean)
